9678861543c754a30b47b93b43c1eb38d4276e1c
Your Name <you@example.com>
2019-10-30 02:18:27 +0300

    Added course gateway

---
+++ gateway-service/src/main/java/com/example/gatewayservice/config/AppConfig.java
@@ -0,0 +1,25 @@
+package com.example.gatewayservice.config;
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableEurekaClient
+@EnableZuulProxy
+public class AppConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+
+}
