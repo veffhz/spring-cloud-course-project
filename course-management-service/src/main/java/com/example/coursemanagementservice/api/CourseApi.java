24d660e976306a497034ad2b1db2a25a38442701
Your Name <you@example.com>
2019-10-30 19:11:10 +0300

    Fixed api, add log, ErrorHandler

--- course-management-service/src/main/java/com/example/coursemanagementservice/api/CourseApi.java
+++ course-management-service/src/main/java/com/example/coursemanagementservice/api/CourseApi.java
@@ -5,6 +5,7 @@ import com.example.coursemanagementservice.model.Transaction;
 import com.example.coursemanagementservice.service.CourseService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -18,6 +19,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class CourseApi {
@@ -32,39 +34,48 @@ public class CourseApi {
     private String serviceId;
 
     @GetMapping("/service/port")
-    public String getPort(){
+    public String getPort() {
+        log.debug("get port");
         return "Service is working at port : " + env.getProperty("local.server.port");
     }
 
     @GetMapping("/service/instances")
     public ResponseEntity<?> getInstances() {
+        log.debug("get instances");
         return ResponseEntity.ok(discoveryClient.getInstances(serviceId));
     }
 
     @GetMapping("/service/user/{userId}")
-    public ResponseEntity<?> findTransactionsOfUser(@PathVariable Long userId){
+    public ResponseEntity<?> findTransactionsOfUser(@PathVariable Long userId) {
+        log.info("get transactions by userId {}", userId);
         return ResponseEntity.ok(courseService.findTransactionsOfUser(userId));
     }
 
     @GetMapping("/service/all")
-    public ResponseEntity<?> findAllCourses(){
+    public ResponseEntity<?> findAllCourses() {
+        log.info("get all curses");
         return ResponseEntity.ok(courseService.allCourses());
     }
 
     @PostMapping("/service/enroll")
     public ResponseEntity<?> saveTransaction(@RequestBody Transaction transaction) {
+        log.info("save transaction, {}", transaction);
         transaction.setDateOfIssue(LocalDateTime.now());
         transaction.setCourse(courseService.findCourseById(transaction.getCourse().getId()));
         return new ResponseEntity<>(courseService.saveTransaction(transaction), HttpStatus.CREATED);
     }
 
     @GetMapping("/service/course/{courseId}")
-    public ResponseEntity<?> findStudentsOfCourse(@PathVariable Long courseId){
+    public ResponseEntity<?> findStudentsOfCourse(@PathVariable Long courseId) {
+        log.info("get students by userId {}", courseId);
         List<Transaction> transactions = courseService.findTransactionsOfCourse(courseId);
-        if(CollectionUtils.isEmpty(transactions)){
+        if (CollectionUtils.isEmpty(transactions)) {
             return ResponseEntity.notFound().build();
         }
-        List<Long> userIdList = transactions.parallelStream().map(Transaction::getUserId).collect(Collectors.toList());
+        List<Long> userIdList = transactions.parallelStream()
+                .map(Transaction::getUserId)
+                .collect(Collectors.toList());
+
         List<String> students = userClient.getUserNames(userIdList);
         return ResponseEntity.ok(students);
     }
---
+++ course-management-service/src/main/java/com/example/coursemanagementservice/api/ErrorHandler.java
@@ -0,0 +1,16 @@
+package com.example.coursemanagementservice.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<String> handle(Exception ex) {
+        return new ResponseEntity<>("Error: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
